92075f7b17a787f819b5d11164af755a7d4c6d10
poonamkothari <poonam.bhgn.kothari@gmail.com>
2026-07-03 12:04:02 +0100

    engineer2 has made some changes

---
+++ tests/Pages/HomePage.spec.ts
@@ -0,0 +1,22 @@
+import{test,expect} from '@playwright/test';
+
+test.describe('Home Page Tests', () => {
+  test('Verify the home page title', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await expect(page).toHaveTitle('OrangeHRM');
+
+  });
+ 
+  test('Verify the home page heading', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    const heading = await page.locator('h5').textContent();
+    expect(heading).toBe('Login');
+  })
+    
+    test('Verify the home page heading by engineer1', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    const heading = await page.locator('h5').textContent();
+    expect(heading).toBe('Login');
+  })
+
+});
