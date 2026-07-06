575406c6909063bf913026de653e4107b28e564d
poonamkothari <poonam.bhgn.kothari@gmail.com>
2026-07-06 09:45:13 +0100

    new test is added

--- tests/Pages/HomePage.spec.ts
+++ tests/Pages/HomePage.spec.ts
@@ -18,5 +18,10 @@ test.describe('Home Page Tests', () => {
     const heading = await page.locator('h5').textContent();
     expect(heading).toBe('Login');
   })
+    test('Verify the home page heading by engineer2', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    const heading = await page.locator('h5').textContent();
+    expect(heading).toBe('Login');
+  })
 
 });
